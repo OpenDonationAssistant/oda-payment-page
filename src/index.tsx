6d77586ffe578203fd1b14aef5c6dfc3c2c1c650
stCarolas <stcarolas@gmail.com>
2024-08-28 22:09:32 +0300

    fix default logo url, use fontsource instead of google fonts cdn:

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+import '@fontsource/black-ops-one';
+import '@fontsource-variable/exo-2';
+import "@fontsource/material-symbols-sharp";
+
 import Donation from "./pages/Donation/Donation";
 
 import PaymentResult, {
