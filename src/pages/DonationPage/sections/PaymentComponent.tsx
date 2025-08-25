c72a74e12c69cbf677340140eec96e12272350bf
stCarolas <stcarolas@gmail.com>
2025-08-26 00:21:57 +0300

    style fixes for crypto buttons

--- src/pages/DonationPage/sections/PaymentComponent.tsx
+++ src/pages/DonationPage/sections/PaymentComponent.tsx
@@ -39,38 +39,44 @@ const PaymentComponent = observer(({}) => {
         )}
         <div className={`${classes.paybuttoncontainer}`}>
           <ODAIcon />
-          {payment.hasFiatGateway && <button
-            className={`${classes.paybutton} ${payment.error ? classes.disabled : ""}`}
-            disabled={payment.error ? payment.error.length > 0 : false}
-            onClick={() =>
-              payment.pay({ type: "fiat" }).then((data) => {
-                const url = data.data.operationUrl as string;
-                if (url.startsWith("http")) {
-                  window.location.href = url;
-                } else {
-                  navigate(data.data.operationUrl);
+          <div className={`${classes.paybuttons}`}>
+            {payment.hasFiatGateway && (
+              <button
+                className={`${classes.paybutton} ${payment.error ? classes.disabled : ""}`}
+                disabled={payment.error ? payment.error.length > 0 : false}
+                onClick={() =>
+                  payment.pay({ type: "fiat" }).then((data) => {
+                    const url = data.data.operationUrl as string;
+                    if (url.startsWith("http")) {
+                      window.location.href = url;
+                    } else {
+                      navigate(data.data.operationUrl);
+                    }
+                  })
                 }
-              })
-            }
-          >
-            Поддержать на {`${payment.amount}\u20BD`}
-          </button>}
-          {payment.hasCryptoGateway && <button
-            className={`${classes.paybutton} ${payment.error ? classes.disabled : ""}`}
-            disabled={payment.error ? payment.error.length > 0 : false}
-            onClick={() =>
-              payment.pay({ type: "crypto" }).then((data) => {
-                const url = data.data.operationUrl as string;
-                if (url.startsWith("http")) {
-                  window.location.href = url;
-                } else {
-                  navigate(data.data.operationUrl);
+              >
+                Поддержать на {`${payment.amount}\u20BD`}
+              </button>
+            )}
+            {payment.hasCryptoGateway && (
+              <button
+                className={`${classes.paybutton} ${payment.error ? classes.disabled : ""}`}
+                disabled={payment.error ? payment.error.length > 0 : false}
+                onClick={() =>
+                  payment.pay({ type: "crypto" }).then((data) => {
+                    const url = data.data.operationUrl as string;
+                    if (url.startsWith("http")) {
+                      window.location.href = url;
+                    } else {
+                      navigate(data.data.operationUrl);
+                    }
+                  })
                 }
-              })
-            }
-          >
-            Поддержать криптовалютой
-          </button>}
+              >
+                Поддержать криптовалютой
+              </button>
+            )}
+          </div>
         </div>
       </div>
     </>
